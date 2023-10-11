fe3bb039c55eb92b5e6887e49f0ca56b6e87d5da
DekayKim <widoint@gmail.com>
2023-10-11 21:42:12 +0900

    초기 설정: site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,65 +1,65 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+    // the site's root Notion page (required)
+    rootNotionPageId: 'bd4842bd31fc4d72adde231f15721005',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+    // if you want to restrict pages to a single notion workspace (optional)
+    // (this should be a Notion ID; see the docs for how to extract this)
+    rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+    // basic site info (required)
+    name: 'Sionic AI Tech Blog',
+    domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+    author: 'Sukhyun Ko',
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+    // open graph metadata (optional)
+    description: 'Create your AI today: simply, quickly, and economically',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+    // social usernames (optional)
+    //   twitter: 'transitive_bs',
+    github: 'sionic-ai',
+    linkedin: 'sionic-ai-inc',
+    // mastodon: '#', // optional mastodon profile URL, provides link verification
+    // newsletter: '#', // optional newsletter URL
+    // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+    // default notion icon and cover images for site-wide consistency (optional)
+    // page-specific values will override these site-wide defaults
+    defaultPageIcon: null,
+    defaultPageCover: null,
+    defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+    // whether or not to enable support for LQIP preview images (optional)
+    isPreviewImageSupportEnabled: true,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
+    // whether or not redis is enabled for caching generated preview images (optional)
+    // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+    // environment variables. see the readme for more info
+    isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+    // map of notion page IDs to URL paths (optional)
+    // any pages defined here will override their default URL paths
+    // example:
+    //
+    pageUrlOverrides: {
+      '/about': 'f9603019252e489bb1228e4ef3ea876f',
+      '/articles': '4e12ecd4a88a4bd9ab811f470a4b1038',
+    },
+    // pageUrlOverrides: null,
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+    // whether to use the default notion navigation style or a custom one with links to
+    // important pages
+    //   navigationStyle: 'default'
+    navigationStyle: 'custom',
+    navigationLinks: [
+        {
+            title: 'About',
+            pageId: 'f9603019252e489bb1228e4ef3ea876f'
+        },
+        {
+            title: 'Articles',
+            pageId: '4e12ecd4a88a4bd9ab811f470a4b1038'
+        }
+    ]
 })
